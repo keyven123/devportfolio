1d0624d8375488e3d1464d970341e1363ceeda16
keyven123 <keyven.rosal@iserviceglobalbiz.com>
2025-04-27 11:27:27 +0800

    remove apostrapy

--- components/Home/Contact/ContactForm.tsx
+++ components/Home/Contact/ContactForm.tsx
@@ -4,7 +4,7 @@ const ContactForm = () => {
   return (
     <div className='bg-[#140c1c] rounded-lg p-4 sm:p-10'>
       <h1 className='text-bg text-2xl md:text-3xl lg:text-[2.5rem] font-bold'>Let's work together!</h1>
-      <p className='text-gray-200 mt-3 lg:text-base text-xs md:text-sm'>If you have any questions or project inquiries, feel free to reach out using the contact details provided. I’m always open to discussing new ideas, collaborations, or opportunities to help bring your vision to life.</p>
+      <p className='text-gray-200 mt-3 lg:text-base text-xs md:text-sm'>If you have any questions or project inquiries, feel free to reach out using the contact details provided. I'm always open to discussing new ideas, collaborations, or opportunities to help bring your vision to life.</p>
     {/* <form className='mt-8 block w-full overflow-hidden'>
       <div className='flex flex-col md:flex-row items-center justify-between gap-4'>
         <input type="text" placeholder='First name' className='flex-1 bg-black text-white placeholder:text-gray-600 px-6 py-6 rounded-md boarder-[1.5px] border-gray-200 border-opacity-15 outline-none w-full' />
